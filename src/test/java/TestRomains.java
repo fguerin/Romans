2f9496c8d2a9e49893244316090d87d4dd58214d
Florian Guérin <floriang33@gmail.com>
2013-03-02 19:21:48 +0100

    Suppression des exceptions inutiles

--- src/test/java/TestRomains.java
+++ src/test/java/TestRomains.java
@@ -8,89 +8,89 @@ public class TestRomains {
     private Romains romains;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         romains = new Romains();
     }
 
     @Test
-    public void leChiffreRomainIDevraitDonner1() throws Exception {
+    public void leChiffreRomainIDevraitDonner1() {
         int nombre = romains.enNombreArabe("I");
 
         assertThat(nombre).isEqualTo(1);
     }
 
     @Test
-    public void leChiffreRomainIIDevraitDonner2() throws Exception {
+    public void leChiffreRomainIIDevraitDonner2() {
         int nombre = romains.enNombreArabe("II");
 
         assertThat(nombre).isEqualTo(2);
     }
 
     @Test
-    public void leChiffreRomainVDevraitDonner5() throws Exception {
+    public void leChiffreRomainVDevraitDonner5() {
         int nombre = romains.enNombreArabe("V");
 
         assertThat(nombre).isEqualTo(5);
     }
 
     @Test
-    public void leChiffreRomainXDevraitDonner10() throws Exception {
+    public void leChiffreRomainXDevraitDonner10() {
         int nombre = romains.enNombreArabe("X");
 
         assertThat(nombre).isEqualTo(10);
     }
 
     @Test
-    public void leChiffreRomainVIDevraitDonner6() throws Exception {
+    public void leChiffreRomainVIDevraitDonner6() {
         int nombre = romains.enNombreArabe("VI");
 
         assertThat(nombre).isEqualTo(6);
     }
 
     @Test
-    public void leChiffreRomainIVDevraitDonner4() throws Exception {
+    public void leChiffreRomainIVDevraitDonner4() {
         int nombre = romains.enNombreArabe("IV");
 
         assertThat(nombre).isEqualTo(4);
     }
 
     @Test
-    public void leChiffreRomainXLIXDevraitDonner49() throws Exception {
+    public void leChiffreRomainXLIXDevraitDonner49() {
         int nombre = romains.enNombreArabe("XLIX");
 
         assertThat(nombre).isEqualTo(49);
     }
 
     @Test
-    public void leChiffreRomainCCCXLVIDevraitDonner346() throws Exception {
+    public void leChiffreRomainCCCXLVIDevraitDonner346() {
         int nombre = romains.enNombreArabe("CCCXLVI");
 
         assertThat(nombre).isEqualTo(346);
     }
 
     @Test
-    public void leChiffreRomainCCCXCIXDevraitDonner399() throws Exception {
+    public void leChiffreRomainCCCXCIXDevraitDonner399() {
         int nombre = romains.enNombreArabe("CCCXCIX");
 
         assertThat(nombre).isEqualTo(399);
     }
 
     @Test
-    public void leChiffreRomainXCIXDevraitDonner99() throws Exception {
+    public void leChiffreRomainXCIXDevraitDonner99() {
         int nombre = romains.enNombreArabe("XCIX");
 
         assertThat(nombre).isEqualTo(99);
     }
 
     @Test
-    public void leChiffreRomainMMMMDCCCLXXXVIIIDevraitDonner4888() throws Exception {
+    public void leChiffreRomainMMMMDCCCLXXXVIIIDevraitDonner4888() {
         int nombre = romains.enNombreArabe("MMMMDCCCLXXXVIII");
 
         assertThat(nombre).isEqualTo(4888);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void leChiffreRomainICDevraitDonnerUneException() throws Exception {
+    public void leChiffreRomainICDevraitDonnerUneException() {
         romains.enNombreArabe("IC");
         romains.enNombreArabe("ID");
         romains.enNombreArabe("IM");
