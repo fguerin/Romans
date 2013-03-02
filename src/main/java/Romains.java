e7c8bfe4467f017c19cd8906fbc5ab2fb3df65e0
Florian Guérin <floriang33@gmail.com>
2013-03-02 19:23:35 +0100

    Reformate du code et renommage

--- src/main/java/Romains.java
+++ src/main/java/Romains.java
@@ -5,17 +5,17 @@ import java.util.Map;
 
 public class Romains {
     Map<String, Integer> romainsVersChiffresArabes = new HashMap<String, Integer>();
-    Map<String, String> aPourUnite = new HashMap<String, String>();
+    Map<String, String> aPourUnité = new HashMap<String, String>();
 
     public Romains() {
         constructionDeLAssociationChiffreRomainValeur();
-        constructionDesUnites();
+        constructionDesUnités();
     }
 
     public int enNombreArabe(String nombreRomain) {
         int nombreArabe = 0;
-        for (int index = 0 ; index < nombreRomain.length() ; ) {
-            if(leChiffreCourantEstPlusPetitQueLeSuivant(nombreRomain, index)) {
+        for (int index = 0; index < nombreRomain.length(); ) {
+            if (leChiffreCourantEstPlusPetitQueLeSuivant(nombreRomain, index)) {
                 verifieQueLeChiffreCourantEstUneUnitéDuSuivant(nombreRomain, index);
                 nombreArabe += chiffreArabeSuivant(nombreRomain, index) - chiffreArabeCourant(nombreRomain, index);
                 index += 2;
@@ -28,7 +28,7 @@ public class Romains {
     }
 
     private void verifieQueLeChiffreCourantEstUneUnitéDuSuivant(String nombreRomain, int index) {
-        Preconditions.checkArgument(verifieUniteDe(chiffreRomainCourant(nombreRomain, index), chiffreRomainSuivant(nombreRomain, index)));
+        Preconditions.checkArgument(vérifieUnitéDe(chiffreRomainCourant(nombreRomain, index), chiffreRomainSuivant(nombreRomain, index)));
     }
 
     private void constructionDeLAssociationChiffreRomainValeur() {
@@ -41,21 +41,21 @@ public class Romains {
         romainsVersChiffresArabes.put("M", 1000);
     }
 
-    private void constructionDesUnites() {
-        aPourUnite.put("V", "I");
-        aPourUnite.put("X", "I");
-        aPourUnite.put("L", "X");
-        aPourUnite.put("C", "X");
-        aPourUnite.put("D", "C");
-        aPourUnite.put("M", "C");
+    private void constructionDesUnités() {
+        aPourUnité.put("V", "I");
+        aPourUnité.put("X", "I");
+        aPourUnité.put("L", "X");
+        aPourUnité.put("C", "X");
+        aPourUnité.put("D", "C");
+        aPourUnité.put("M", "C");
     }
 
     private String chiffreRomainSuivant(String roman, int index) {
-        return roman.substring(index+1, index+2);
+        return roman.substring(index + 1, index + 2);
     }
 
     private String chiffreRomainCourant(String roman, int index) {
-        return roman.substring(index, index+1);
+        return roman.substring(index, index + 1);
     }
 
     private boolean leChiffreCourantEstPlusPetitQueLeSuivant(String roman, int index) {
@@ -71,10 +71,10 @@ public class Romains {
     }
 
     private boolean leNombreRomainsAChiffreSuivant(String roman, int index) {
-        return index+1 < roman.length();
+        return index + 1 < roman.length();
     }
 
-    private boolean verifieUniteDe(String unite, String chiffre) {
-        return unite.equals(aPourUnite.get(chiffre));
+    private boolean vérifieUnitéDe(String unité, String chiffre) {
+        return unité.equals(aPourUnité.get(chiffre));
     }
 }
